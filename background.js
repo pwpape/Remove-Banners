19fa56fd010f27b173aa9c1af98ae56a3974a947
Patrick Pape <patrick.pape@jjventures.com>
2022-02-11 15:58:39 -0600

    removed them; didn't work

--- background.js
+++ background.js
@@ -1,29 +1,6 @@
 
 chrome.tabs.executeScript(tabs[0].id, {file: "content.js"});
 
-chrome.webRequest.onBeforeSendHeaders.addEventListener(changeCookies, {
-        urls: ["<all_urls>"],
-        types: ["main_frame"], },
-    ["requestHeaders", "blocking", "extraHeaders"]
-);
+// https://medium.datadriveninvestor.com/how-to-bypass-any-paywall-for-free-df87832cbff7
 
-function changeCookies(details) {
-    details.requestHeaders = details.requestHeaders.filter(header => {
-        if (header.name === "Referer") {
-            return false;
-        }
-    })
 
-    details.requestHeaders.push({
-        "name": "Referer",
-        "value": "https://t.co/"
-    })
-
-    console.log("Changed request header to Twitter");
-
-    let requestHeadersObject = {
-        requestHeaders: details.requestHeaders
-    };
-
-    return requestHeadersObject;
-}
