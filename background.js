8d89692f5c08d7f7c30abcd03743a179859d8b08
Patrick Pape <patrick.pape@jjventures.com>
2022-02-11 15:45:11 -0600

    added requestHeader function to background.js

--- background.js
+++ background.js
@@ -1,2 +1,29 @@
 
 chrome.tabs.executeScript(tabs[0].id, {file: "content.js"});
+
+chrome.webRequest.onBeforeSendHeaders.addEventListener(changeCookies, {
+        urls: ["<all_urls>"],
+        types: ["main_frame"], },
+    ["requestHeaders", "blocking", "extraHeaders"]
+);
+
+function changeCookies(details) {
+    details.requestHeaders = details.requestHeaders.filter(header => {
+        if (header.name === "Referer") {
+            return false;
+        }
+    })
+
+    details.requestHeaders.push({
+        "name": "Referer",
+        "value": "https://t.co/"
+    })
+
+    console.log("Changed request header to Twitter");
+
+    let requestHeadersObject = {
+        requestHeaders: details.requestHeaders
+    };
+
+    return requestHeadersObject;
+}
